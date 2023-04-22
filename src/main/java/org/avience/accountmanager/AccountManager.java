da88762f4b677a351bb5417417ac83ee7258f35c
Rob <95727104+mnfu@users.noreply.github.com>
2023-04-21 22:17:45 -0400

    rework some dependencies

--- src/main/java/org/avience/accountmanager/AccountManager.java
+++ src/main/java/org/avience/accountmanager/AccountManager.java
@@ -4,6 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AccountManager extends JavaPlugin {
 
+    public static void main(String[] args) {
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
