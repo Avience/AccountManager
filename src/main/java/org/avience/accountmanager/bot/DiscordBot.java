803d91ae0b5d0ccbb414d0c858eef327794b0906
Rob <95727104+mnfu@users.noreply.github.com>
2023-04-22 20:34:01 -0400

    get bot to load

---
+++ src/main/java/org/avience/accountmanager/bot/DiscordBot.java
@@ -0,0 +1,14 @@
+package org.avience.accountmanager.bot;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class DiscordBot {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String botToken = dotenv.get("DISCORD_TOKEN");
+        System.out.println(botToken);
+        JDA jda = JDABuilder.createDefault(botToken).build();
+    }
+}
