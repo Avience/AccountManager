71bd1ee22ab8a9b1806d92d3023de69d4ad647d9
reaganlittle <raygelbaygel@gmail.com>
2023-04-26 20:16:09 -0400

    figured out how to limit commands to certain roles

--- src/main/java/org/avience/accountmanager/bot/commands/TestCommands.java
+++ src/main/java/org/avience/accountmanager/bot/commands/TestCommands.java
@@ -1,5 +1,6 @@
 package org.avience.accountmanager.bot.commands;
 
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -28,8 +29,40 @@ public class TestCommands extends ListenerAdapter {
             // use .deferReply to make discord wait for commands that take a long time to execute
         }
 
-        else if (command.equals("whitelist")) { // experiment with deferreply(), THE COMMANDS WILL NOT BE AN IF ELSE STATEMENTS IN PROD
+        else if (command.equals("secret")) { // experiment with deferreply(), THE COMMANDS WILL NOT BE AN IF ELSE STATEMENTS IN PROD
 
+            //BotUtils botUtils = new BotUtils()
+            //boolean yay = event.getMember().hasR
+
+            long CoOwnerRoleID = 1099014264775245874L;
+            //boolean hasRole;
+            Role role = event.getGuild().getRoleById(CoOwnerRoleID); //will return null if command is run in a server besides account manager test server
+            if (event.getMember().getRoles() == null || event.getMember().getRoles().contains(role) == false) {
+                //hasRole = false;
+                event.reply(event.getUser().getName() + " can't use the secret command!").queue();
+            } else {
+                //hasRole = event.getMember().getRoles().contains(role);
+                //System.out.println();
+                event.reply(event.getUser().getName() + " has the " + role.getName() + " role and can use the secret command!").queue();
+            }
+
+
+
+
+            // janky ass thingy that is a role-specific command - doesnt work
+            /*boolean hasRole = false;
+            List<Role> memberRoles = event.getMember().getRoles();
+            //List<Long> memberRoleIDs;
+            for (Role role : memberRoles) {
+                 if (role.getIdLong() == 1099014264775245874L) { //role.getid equals the admin role
+                     hasRole = true;
+                 }
+            }
+            if (hasRole) {
+                event.reply(event.getUser().getName() + " has the " + "[role]" + " role and can use the secret command!");
+            }*/
+            //if (event.getMember().)
+            //event.getMember().getRoles()
         }
     }
 
@@ -39,7 +72,8 @@ public class TestCommands extends ListenerAdapter {
     public void onGuildReady(@NotNull GuildReadyEvent event) {
         List<CommandData> commandData = new ArrayList<>();
         commandData.add(Commands.slash("ping", "ping pong"));
-
+        commandData.add(Commands.slash("secret", "only members with admin can execute this command"));
+        event.getGuild().updateCommands().addCommands(commandData).queue();
 
         /*
          if (event.getGuild().getIdLong() == 1099013320272527420L) {
@@ -48,7 +82,6 @@ public class TestCommands extends ListenerAdapter {
          }
          */
         // you can put all the commands in with a list (current implementation), or you can put them in individually, separated by commas
-        event.getGuild().updateCommands().addCommands(commandData).queue();
     }
     // NOT FINAL IMPLEMENTATION!!!!! WILL MAKE A COMMAND MANAGER SYSTEM
     // THIS SYSTEM IS FOR TESTING ONLY
@@ -56,6 +89,7 @@ public class TestCommands extends ListenerAdapter {
     public void onGuildJoin(@NotNull GuildJoinEvent event) {
         List<CommandData> commandData = new ArrayList<>();
         commandData.add(Commands.slash("ping", "ping pong"));
+        commandData.add(Commands.slash("secret", "only members with admin can execute this command"));
         event.getGuild().updateCommands().addCommands(commandData).queue();
     }
 }
